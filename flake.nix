4f62326314cc399d198a9f14298ea96a35be38af
Ran Xiao <ben.xiao@me.com>
2022-08-23 21:56:38 +1000

    reformatting

--- flake.nix
+++ flake.nix
@@ -160,15 +160,15 @@
               };
               services.xserver.displayManager.gdm.autoSuspend = true;
               security.polkit.extraConfig = ''
-                  polkit.addRule(function(action, subject) {
-                        if (action.id == "org.freedesktop.login1.suspend" ||
-                          action.id == "org.freedesktop.login1.suspend-multiple-sessions" ||
-                          action.id == "org.freedesktop.login1.hibernate" ||
-                          action.id == "org.freedesktop.login1.hibernate-multiple-sessions")
-                      {
-                            return polkit.Result.NO;
-                      }
-                  });
+                polkit.addRule(function(action, subject) {
+                  if (action.id == "org.freedesktop.login1.suspend" ||
+                    action.id == "org.freedesktop.login1.suspend-multiple-sessions" ||
+                    action.id == "org.freedesktop.login1.hibernate" ||
+                    action.id == "org.freedesktop.login1.hibernate-multiple-sessions")
+                  {
+                    return polkit.Result.NO;
+                  }
+                });
               '';
               hardware.nvidia.nvidiaPersistenced = true;
               boot.initrd.postDeviceCommands = ''
