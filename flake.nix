cf65d3af6c641aced805fcde399f6329fae0d7f5
Ran Xiao <ben.xiao@me.com>
2022-10-05 21:50:32 +1100

    more refactoring

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "my computers in flakes";
+  description = "all my machines in flakes";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
   inputs.nixpkgs-master.url = "github:nixos/nixpkgs";
   inputs.vscode-server.url = "github:msteen/nixos-vscode-server";
@@ -12,7 +12,7 @@
             inherit system;
             config.allowUnfree = true;
           };
-          
+
           makeNvidiaModule = { powerlimit }: ({ pkgs, lib, modulesPath, ... }: {
             services.xserver.videoDrivers = [ "nvidia" ];
             hardware.nvidia.nvidiaPersistenced = true;
@@ -32,7 +32,7 @@
 
           simplesystem =
             { hostName
-            , extraModules ? []
+            , extraModules ? [ ]
             , rootPool ? "zroot/root"
             , bootDevice ? "/dev/nvme0n1p3"
             , swapDevice ? "/dev/nvme0n1p2"
@@ -40,7 +40,6 @@
               inherit system;
               modules =
                 [
-                  vscode-server.nixosModule
 
                   ({ pkgs, lib, modulesPath, ... }:
                     {
@@ -157,70 +156,74 @@
           # Lenovo T490
           apollo = nixpkgs.lib.nixosSystem (simplesystem {
             hostName = "apollo";
-            extraModules = [({ pkgs, lib, modulesPath, ... }:
-              {
-                services.tailscale.enable = true;
-                powerManagement.cpuFreqGovernor = "powersave";
-                hardware.cpu.intel.updateMicrocode = true;
-                environment.interactiveShellInit = ''
-                  alias athena='ssh rxiao@192.168.50.187'
-                  alias artemis='ssh rxiao@artemis.silverpond.com.au'
-                  export RUST_BACKTRACE=1
-                '';
+            extraModules = [
+              ({ pkgs, lib, modulesPath, ... }:
+                {
+                  services.tailscale.enable = true;
+                  powerManagement.cpuFreqGovernor = "powersave";
+                  hardware.cpu.intel.updateMicrocode = true;
+                  environment.interactiveShellInit = ''
+                    alias athena='ssh rxiao@192.168.50.187'
+                    alias artemis='ssh rxiao@artemis.silverpond.com.au'
+                    export RUST_BACKTRACE=1
+                  '';
 
-                environment.systemPackages = with pkgs; [ mongodb-compass vscode remmina ];
-                virtualisation.virtualbox.host.enable = true;
-                virtualisation.virtualbox.host.enableExtensionPack = true;
-                users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
+                  environment.systemPackages = with pkgs; [ mongodb-compass vscode remmina ];
+                  virtualisation.virtualbox.host.enable = true;
+                  virtualisation.virtualbox.host.enableExtensionPack = true;
+                  users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
 
-              })
-              ];
+                })
+            ];
 
           });
           # amd ryzen 7 1700
           athena = nixpkgs.lib.nixosSystem (simplesystem {
             hostName = "athena";
-            extraModules = [({ pkgs, lib, modulesPath, ... }:
-              {
-                services.vscode-server.enable = true;
-                hardware.cpu.amd.updateMicrocode = true;
-                boot.kernelModules = [ "kvm-amd" ];
-                services.openssh = {
-                  enable = true;
-                  passwordAuthentication = false;
-                };
-                services.xserver.displayManager.gdm.autoSuspend = false;
-                security.polkit.extraConfig = ''
-                  polkit.addRule(function(action, subject) {
-                    if (action.id == "org.freedesktop.login1.suspend" ||
-                      action.id == "org.freedesktop.login1.suspend-multiple-sessions" ||
-                      action.id == "org.freedesktop.login1.hibernate" ||
-                      action.id == "org.freedesktop.login1.hibernate-multiple-sessions")
-                    {
-                      return polkit.Result.NO;
-                    }
-                  });
-                '';
-                hardware.nvidia.nvidiaPersistenced = true;
-                boot.initrd.postDeviceCommands = ''
-                  zpool import -f data
-                  zpool import -f torrents
-                '';
-                environment.systemPackages = with pkgs; [ bpytop nethogs qbittorrent-nox ];
-                systemd.services.qbittorrent-server = {
-                  wantedBy = [ "multi-user.target" ];
-                  description = "qbittorrent webserver";
-                  serviceConfig = {
-                    Type = "simple";
-                    ExecStart = ''
-                      /run/current-system/sw/bin/qbittorrent-nox --webui-port=8083                   
-                    '';
+            extraModules = [
+              ({ pkgs, lib, modulesPath, ... }:
+                {
+                  services.vscode-server.enable = true;
+                  hardware.cpu.amd.updateMicrocode = true;
+                  boot.kernelModules = [ "kvm-amd" ];
+                  services.openssh = {
+                    enable = true;
+                    passwordAuthentication = false;
                   };
-                };
-              })
-              
-              makeNvidiaModule { powerlimit=205; }
-              ];
+                  services.xserver.displayManager.gdm.autoSuspend = false;
+                  security.polkit.extraConfig = ''
+                    polkit.addRule(function(action, subject) {
+                      if (action.id == "org.freedesktop.login1.suspend" ||
+                        action.id == "org.freedesktop.login1.suspend-multiple-sessions" ||
+                        action.id == "org.freedesktop.login1.hibernate" ||
+                        action.id == "org.freedesktop.login1.hibernate-multiple-sessions")
+                      {
+                        return polkit.Result.NO;
+                      }
+                    });
+                  '';
+                  hardware.nvidia.nvidiaPersistenced = true;
+                  boot.initrd.postDeviceCommands = ''
+                    zpool import -f data
+                    zpool import -f torrents
+                  '';
+                  environment.systemPackages = with pkgs; [ bpytop nethogs qbittorrent-nox ];
+                  systemd.services.qbittorrent-server = {
+                    wantedBy = [ "multi-user.target" ];
+                    description = "qbittorrent webserver";
+                    serviceConfig = {
+                      Type = "simple";
+                      ExecStart = ''
+                        /run/current-system/sw/bin/qbittorrent-nox --webui-port=8083                   
+                      '';
+                    };
+                  };
+                })
+
+              vscode-server.nixosModule
+              makeNvidiaModule
+              { powerlimit = 205; }
+            ];
           });
           # amd ryzen 7 3700x
           wotan = nixpkgs.lib.nixosSystem (simplesystem {
@@ -228,46 +231,49 @@
             swapDevice = "/dev/disk/by-uuid/79ef359f-1882-4427-a93e-363259bc2445";
             bootDevice = "/dev/disk/by-uuid/07D2-41D4";
             extraModules = [
-              makeNvidiaModule { powerlimit=125; }
+              makeNvidiaModule
+              { powerlimit = 125; }
             ];
           });
           # amd ryzen 3950x
           dante = nixpkgs.lib.nixosSystem (simplesystem {
             hostName = "dante";
-            extraModules = [({ pkgs, lib, modulesPath, ... }:
-              {
-                services.tailscale.enable = true;
-                hardware.cpu.amd.updateMicrocode = true;
-                nix.settings = {
-                  substituters = [ "https://hydra.iohk.io" "https://iohk.cachix.org" ];
-                  trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo=" ];
-                };
-                boot.kernelModules = [ "kvm-amd" ];
-                environment.interactiveShellInit = ''
-                  alias athena='ssh rxiao@192.168.50.187'
-                  alias artemis='ssh rxiao@artemis.silverpond.com.au'
-                  export RUST_BACKTRACE=1
-                '';
-                boot.initrd.postDeviceCommands = ''
-                  zpool import -f zdata
-                  zpool import -f bigdisk
-                '';
-                services.openssh = {
-                  enable = true;
-                  passwordAuthentication = true;
-                };
-                virtualisation.virtualbox.host.enable = true;
-                virtualisation.virtualbox.host.enableExtensionPack = true;
-                users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
-                programs.steam = {
-                  enable = true;
-                  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-                  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-                };
-                environment.systemPackages = with pkgs; [ mongodb-compass master.vscode remmina ];
-              })
-              makeNvidiaModule {powerlimit=205; }
-              ];
+            extraModules = [
+              ({ pkgs, lib, modulesPath, ... }:
+                {
+                  services.tailscale.enable = true;
+                  hardware.cpu.amd.updateMicrocode = true;
+                  nix.settings = {
+                    substituters = [ "https://hydra.iohk.io" "https://iohk.cachix.org" ];
+                    trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo=" ];
+                  };
+                  boot.kernelModules = [ "kvm-amd" ];
+                  environment.interactiveShellInit = ''
+                    alias athena='ssh rxiao@192.168.50.187'
+                    alias artemis='ssh rxiao@artemis.silverpond.com.au'
+                    export RUST_BACKTRACE=1
+                  '';
+                  boot.initrd.postDeviceCommands = ''
+                    zpool import -f zdata
+                    zpool import -f bigdisk
+                  '';
+                  services.openssh = {
+                    enable = true;
+                    passwordAuthentication = true;
+                  };
+                  virtualisation.virtualbox.host.enable = true;
+                  virtualisation.virtualbox.host.enableExtensionPack = true;
+                  users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
+                  programs.steam = {
+                    enable = true;
+                    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+                    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+                  };
+                  environment.systemPackages = with pkgs; [ mongodb-compass master.vscode remmina ];
+                })
+              makeNvidiaModule
+              { powerlimit = 205; }
+            ];
           });
         };
     };
