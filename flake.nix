ab38f0fdc7451e29b97353522bf1f5141cb1dc04
Ran Xiao <ben.xiao@me.com>
2023-01-28 14:28:41 +1100

    update video player

--- flake.nix
+++ flake.nix
@@ -43,7 +43,7 @@
               gnome-console
               gnome.gnome-chess
               stockfish
-              celluloid
+              mpv
               vlc
               firefox
               thunderbird
