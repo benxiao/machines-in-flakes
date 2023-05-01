681444748fccd1aa136a87bb359c1cec291390cb
Ran Xiao <ben.xiao@me.com>
2023-05-01 20:00:04 +1000

    use buildFHSEnv to have a ubuntu experience with python env

---
+++ python_templates/buildFHSEnv/flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Ultralytics";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShells.default = (pkgs.buildFHSUserEnv {
+          name = "pipzone";
+          targetPkgs = pkgs: (with pkgs; [
+            python3
+            python3.pkgs.pip
+            poetry
+            python3.pkgs.virtualenv
+            gcc
+            pkg-config
+            cairo.dev
+            # for opencv
+            xorg.libXext
+            xorg.libSM
+            xorg.libxcb.dev
+            xorg.libICE
+            xorg.libX11.dev
+            xorg.xorgproto
+            glib.dev
+            gobject-introspection.dev
+            libffi.dev
+            cmake
+            zlib
+            libglvnd
+          ]);
+          runScript = "bash";
+        }).env;
+    });
+}
