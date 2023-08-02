28b624b2e9271f91835d7cad50508144657821e7
rxiao <ran.xiao@silverpond.com.au>
2023-08-02 13:44:02 +1000

    update python template

--- python_templates/flake.nix
+++ python_templates/flake.nix
@@ -24,26 +24,34 @@
                 packageOverrides = pkgs: rec{
                   # override package
                   # thrift = pkgs.thrift.overrideAttrs (old: { doCheck = false; });
-                  python3 = pkgs.python3.override {
-                    # super important
-                    self = python3;
-                    packageOverrides = self: super: {
-                      # override python packages
-                      # torch = super.torch-bin;
-                      # torchvision = super.torchvision-bin;
-                      # numpy = changeVersion super.numpy.overridePythonAttrs "1.23.5" "sha256-Gxdm1vOXwYFT1AAV3fx53bcVyrrcBNLSKNTlqLxN7Ro=";
-                    };
-                  };
+
+                  
                 };
               };
             };
+      
+            my-python = pkgs-python.python310.override {
+              # super important
+              self = my-python;
+              packageOverrides = self: super: {
+                mmengine = super.mmengine.overridePythonAttrs(old: { doCheck = false; nativeBuildInputs = []; });
+                # override python packages
+                # torch = super.torch-bin;
+                # torchvision = super.torchvision-bin;
+                # numpy = changeVersion super.numpy.overridePythonAttrs "1.23.5" "sha256-Gxdm1vOXwYFT1AAV3fx53bcVyrrcBNLSKNTlqLxN7Ro=";
+              };
+            };
+
+            
           in
             pkgs.mkShell {
               buildInputs = [
                 pkgs.just
-                (pkgs-python.python3.withPackages (p: with p; [
+                (my-python.withPackages (p: with p; [
                   # pandas
                   # cramjam
+                  mmengine
+                  pytest
                 ]))
               ];
             };
