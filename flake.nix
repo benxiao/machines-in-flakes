a96bcb2498f541f2a80bb37010df73a2f55fd102
ran xiao <ben.xiao@me.com>
2024-01-26 10:56:25 +1100

    remove wotan, as it is a windows machine now

--- flake.nix
+++ flake.nix
@@ -251,7 +251,6 @@
                   services.xserver.displayManager.gdm.wayland = false;
                   environment.interactiveShellInit = ''
                     alias athena='ssh rxiao@192.168.50.144'
-                    alias wotan='ssh rxiao.asuscomm.com -p 14285'
                     export RUST_BACKTRACE=1
                   '';
                 })
